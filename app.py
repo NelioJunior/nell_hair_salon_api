922f9f70b76677c1c0fa316e62be64e702320530
nelljr <nelljunior@hotmail.com>
2024-02-18 20:51:40 -0300

    workin on customer_service endpoint

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import openai
 from neural import nucleoNeural
 import locale
@@ -15,7 +16,15 @@ message_info = {
                 "message" : "",
                 "lastMessageTime": datetime.now().strftime("%H:%M"),  
                 "pasta": "https://nelltek.ddns.net/nellSite/ClientesParceirosNell/gestorPai_SalaoConsultorioMVC/" 
-}  
+}
+
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY")) 
+
+rule_file = "./angel_guide.txt"
+rule = open(rule_file, "r")  
+system_msg = rule.read()
+system_msg = system_msg.replace('\t', ' ')     
+system_msg = system_msg.replace('\n', ' ')     
 
 dba_content = (
     "You are a MySQL database administrator, and your task is to create SQL statements that help answer user questions."
@@ -36,21 +45,27 @@ def root():
 
 @app.route("/customer_service", methods=['POST'])
 def customer_service():
-    global previous_msg 
-
     data = request.get_json()
-
-    message_info["requester"] = data.get('requester') 
     message_info["message"] = data.get('question') 
-    response = nucleoNeural(message_info) 
+    message_info["requester"] = data.get('requester') 
 
-    messages=[
-        {"role": "system", "content": dba_content},
-        {"role": "user", "content": message_info["message"]},
+    response = nucleoNeural(message_info) 
+    response = f"user: {response}"
+
+    message=[
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": response}
     ]
 
-    return jsonify({'answer': response})
+    # chat_completion = client.chat.completions.create(
+    #     messages=message,
+    #     model="gpt-3.5-turbo",
+    #     temperature=0.7
+    # )
 
+    # response = chat_completion.choices[0].message.content
+
+    return jsonify({'answer': response})
 
 if __name__ ==  '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)    
