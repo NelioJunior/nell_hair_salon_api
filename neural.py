dd03a74e983348e8b99fff48c7e08e21ff55f468
nelljr <nelljunior@hotmail.com>
2024-03-21 19:05:07 -0300

    refacture - nucleoNeural()

--- neural.py
+++ neural.py
@@ -20,7 +20,6 @@ def processamentoDeLinguagemNatural(mensagemOriginal):
 
 def nucleoNeural(message_info):
    reply_msg = message_info["message"].lower() 
-   contato = message_info["user"]  
    clsModel = model.model(message_info["pasta"])
    pnl = processamentoDeLinguagemNatural(reply_msg)
    mensagemTraduzida = pnl[0]
@@ -28,17 +27,16 @@ def nucleoNeural(message_info):
    
    respBaseConhecimento.append(pnl[0])   
    if pnl[1] == 'confirmacao': 
-      respBaseConhecimento.append(pnl[1])   
+      respBaseConhecimento.append(pnl[1])  
+   elif pnl[1] == 'encerrar': 
+      respBaseConhecimento.append('naoRelacionado')  
    else:
       respBaseConhecimento.append(message_info["detected"])   
 
-   return_msg = clsModel.execute(states,message_info,respBaseConhecimento,mensagemTraduzida)
-
-   if not return_msg or 'naoRelacionado' in respBaseConhecimento[1]:
+   if 'naoRelacionado' in respBaseConhecimento[1]:
       reply_msg  = "responda a mensagem da(o) cliente da maneira mais adequada possivel."   
-   elif 'json:' in return_msg:  
-      reply_msg  = 'Verifique na lista (json) se temos o que a/o cliente quer ... %s' % return_msg
    else:   
-      reply_msg  = f'Diga exatamente as mesmas palavras ... {return_msg}'  
+      return_msg = clsModel.execute(states,message_info,respBaseConhecimento,mensagemTraduzida)
+      reply_msg  = f'Diga exatamente com as mesmas palavras ... {return_msg}'  
 
    return reply_msg
