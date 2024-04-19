2af41824905635934bec68a069e9826d874c3a5c
nelljr <nelljunior@hotmail.com>
2024-04-18 22:47:46 -0300

    fixed smallphone layout

--- static/vendor/js/chat.js
+++ static/vendor/js/chat.js
@@ -2,11 +2,10 @@ const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
-const sendChatBtn = document.querySelector(".chat-input span");
+const sendChatBtn = document.querySelector("#send-btn");
 const inputInitHeight = chatInput.scrollHeight;
 const entity = window.location.pathname.split('/').pop().replace("lista", "").replace(".php", "") ;
 const warning = "Me desculpa,nao entendi...";
-const pageTitle = document.querySelector('.breadcrumb-item.active h4');
 let title = "" 
 let avatar = "brunetteHair"
 
@@ -15,15 +14,11 @@ const createChatLi = (message, className) => {
     chatLi.classList.add("chat", `${className}`);
 
     let chatContent = "<p></p>";
-    
-    if (className === "incoming" && title !== "Agendamento") {
-        chatContent = `<img src="./img/${avatar}.png" class="profile-image"><p></p>`
-    }  
 
     chatLi.innerHTML = chatContent;
 
     if (message === "image"){
-        chatLi.querySelector("p").innerHTML = "<div class='thinking-div'><img src='./img/typing.gif' class='thinking-image'></div>";
+        chatLi.querySelector("p").innerHTML = "<div class='thinking-div'><img src='/static/img/typing.gif' class='thinking-image'></div>";
     } else {
         chatLi.querySelector("p").textContent = message;
     }
@@ -31,7 +26,7 @@ const createChatLi = (message, className) => {
 }
 
 function callCustomerServiceApi (chatbox) {
-    const urlApi = "https://5e6hykidwl7wfbutf5i3tbq7fq0xoziv.lambda-url.us-east-2.on.aws/"
+    const urlApi = "http://192.168.0.59:8000/customer_service"
     const outgoing_lst = chatbox.querySelectorAll(".outgoing");
     const msg = outgoing_lst[outgoing_lst.length-1].innerText;
     const user = selChatCustomer.options[selChatCustomer.selectedIndex].text
@@ -59,35 +54,6 @@ function callCustomerServiceApi (chatbox) {
     });
 }
 
-function callBiApi (chatbox) {
-    const urlApi = "https://7szomtln4cyzvrlbznxxnh64au0evtgq.lambda-url.us-east-2.on.aws/";
-    const outgoing_lst = chatbox.querySelectorAll(".outgoing");
-    const last_user_msg = outgoing_lst[outgoing_lst.length-1].innerText;
-
-    fetch(urlApi, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            question: last_user_msg,
-            entity: entity
-        }),
-    })
-    .then(response => response.json())
-    .then(result => {
-        incoming_lst = chatbox.querySelectorAll(".chat.incoming");
-        last_bot_message = incoming_lst[incoming_lst.length-1].querySelector('p');
-        last_bot_message.innerText = result['answer']
-    })
-    .catch(error => {
-        incoming_lst = chatbox.querySelectorAll(".chat.incoming");
-        last_bot_message = incoming_lst[incoming_lst.length-1].querySelector('p');
-        last_bot_message.innerText = warning
-    });
-
-}
-
 selChatCustomer.addEventListener('change', function() {
     chatbox.innerHTML = ""
 });
@@ -107,13 +73,7 @@ const handleChat = () => {
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
 
-        if (pageTitle) {
-            if (title == "Agendamento") {
-                callCustomerServiceApi(chatbox);
-            } else {
-                callBiApi (chatbox)
-            }             
-        }
+        callCustomerServiceApi(chatbox);
         
     }, 600);
 }
@@ -121,6 +81,11 @@ const handleChat = () => {
 chatInput.addEventListener("input", () => {
     chatInput.style.height = `${inputInitHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+sendChatBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    handleChat();
 });
 
 chatInput.addEventListener("keydown", (e) => {
