f76235d9397cc6e63a8da701f2673210d8d4bb67
NellJr <nelljunior@hotmail.com>
2024-05-16 22:27:39 -0300

    updated sql_ai.py to get the api key

--- sql_ai.py
+++ sql_ai.py
@@ -1,8 +1,12 @@
+from tools import obter_chave_openai
+
 from datetime import datetime 
 
 from langchain_community.utilities import SQLDatabase
 from langchain.chains import create_sql_query_chain
 from langchain_openai import ChatOpenAI
+
+chave_openai = obter_chave_openai()
 
 db_user = "root"
 db_password = "2246"
@@ -10,7 +14,7 @@ db_host = "177.143.21.214"
 db_name = "SalaoConsultorio"
 db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
 
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(api_key=chave_openai,  model="gpt-3.5-turbo", temperature=0)
 chain = create_sql_query_chain(llm, db)
 
 def ask_the_database(query):
